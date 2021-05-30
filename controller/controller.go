5d5daa82c5b03f56d61d07ac6edbfed71b12526e
Merey Tursyn <imerei777@gmail.com>
2021-05-30 22:27:16 +0600

    Fix ExtractTokenMetadata

--- controller/controller.go
+++ controller/controller.go
@@ -257,3 +257,15 @@ func Logout(c *gin.Context) {
 
 	c.JSON(http.StatusOK, "Successfully logged out")
 }
+
+func TokenAuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		err := TokenValid(c.Request)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, err.Error())
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
